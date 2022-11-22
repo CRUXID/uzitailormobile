0701b5088c8b4fed5b3b1a5a79fdeb9f4f4951a0
gabriellia02 <gabrielliaalfina2002@gmail.com>
2022-11-22 21:04:05 +0700

    gabriellia

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -30,7 +30,7 @@ class _DashboardState extends State<Dashboard> {
           Container(
             // margin: EdgeInsets.only(top:),
             width: double.infinity,
-            height: 200,
+            height: 250,
             decoration: BoxDecoration(
               color: Color(0xFFEF233C),
               borderRadius: BorderRadius.only(
@@ -40,7 +40,7 @@ class _DashboardState extends State<Dashboard> {
           ),
           Container(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              margin: EdgeInsets.only(top: 50, left: 50, right: 50),
+              margin: EdgeInsets.only(top: 80, left: 50, right: 50),
               width: double.infinity,
               height: 100,
               decoration: BoxDecoration(
@@ -50,6 +50,71 @@ class _DashboardState extends State<Dashboard> {
                     BoxShadow(
                         color: Colors.black38, spreadRadius: 0.1, blurRadius: 5)
                   ])),
+          Padding(
+            padding: EdgeInsets.fromLTRB(50, 280, 0, 0),
+            child: Text(
+              'Status Pesanan',
+              style: TextStyle(
+                  color: Color(0xFF8D99AE),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300),
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.fromLTRB(15, 305, 15, 0),
+              child: Divider(
+                color: Color(0xFF8D99AE),
+                thickness: 1,
+              )),
+          Padding(
+            padding: EdgeInsets.fromLTRB(15, 330, 0, 0),
+            child: ElevatedButton(
+              child: const Text('1'),
+              style: TextButton.styleFrom(
+                  backgroundColor: Color(0xFF8D99AE),
+                  padding: EdgeInsets.symmetric(horizontal: 45, vertical: 40),
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(5.0))),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(140, 330, 0, 0),
+            child: ElevatedButton(
+              child: const Text('2'),
+              style: TextButton.styleFrom(
+                  backgroundColor: Color(0xFF8D99AE),
+                  padding: EdgeInsets.symmetric(horizontal: 45, vertical: 40),
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(5.0))),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(270, 330, 15, 0),
+            child: ElevatedButton(
+              child: const Text('3'),
+              style: TextButton.styleFrom(
+                  backgroundColor: Color(0xFF8D99AE),
+                  padding: EdgeInsets.symmetric(horizontal: 45, vertical: 40),
+                  shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(5.0))),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(10, 420, 15, 0),
+            child: TextButton(
+              child: const Text(
+                'Tunggu Konfirmasi',
+                style: TextStyle(
+                  color: Color(0xFF8D99AE),
+                  fontSize: 10,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ),
         ]));
   }
 }
--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
