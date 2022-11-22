bd94c08493610713ec13b3bc2b75bbda714e4e98
gabriellia02 <gabrielliaalfina2002@gmail.com>
2022-11-22 19:30:23 +0700

    gabriel

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -7,16 +7,49 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-        height: size.height,
-        width: size.width,
-        child: Stack(alignment: Alignment.center, children: <Widget>[
-          Positioned(
-            top: 0,
-            child: Image.asset('Ellipse 1.png'),
-            width: size.width * 0.3,
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Stack(children: [
+          SizedBox(
+            height: 80,
           ),
+          Text(
+            "Status Pesanan",
+            style: TextStyle(
+                color: Color(0xFF8D99AE),
+                fontWeight: FontWeight.w300,
+                fontSize: 17),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Divider(
+            color: Color(0xFF8D99AE),
+            thickness: 1,
+          ),
+          Container(
+            // margin: EdgeInsets.only(top:),
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Color(0xFFEF233C),
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(150),
+                  bottomLeft: Radius.circular(150)),
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              margin: EdgeInsets.only(top: 50, left: 50, right: 50),
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                  color: Color(0xFF414460),
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black38, spreadRadius: 0.1, blurRadius: 5)
+                  ])),
         ]));
   }
 }
