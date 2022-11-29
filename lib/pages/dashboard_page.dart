c7076aea3374fb33ff7e3d9ff1d3c9fecb91a822
gabriellia02 <gabrielliaalfina2002@gmail.com>
2022-11-29 20:52:41 +0700

    Gabriellia

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -42,22 +42,61 @@ class _DashboardState extends State<Dashboard> {
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
               margin: EdgeInsets.only(top: 80, left: 50, right: 50),
               width: double.infinity,
-              height: 100,
+              height: 120,
               decoration: BoxDecoration(
                   color: Color(0xFF414460),
                   borderRadius: BorderRadius.circular(12),
                   boxShadow: [
                     BoxShadow(
                         color: Colors.black38, spreadRadius: 0.1, blurRadius: 5)
-                  ])),
+                  ]),
+              child: Column(children: [
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'On Going Transaction',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Divider(
+                  color: Color(0xFF8D99AE),
+                  thickness: 1,
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Estimasi Pesanan Selesai',
+                  style: TextStyle(
+                      color: Color(0xFF8D99AE),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Rabu, 10 Oktober 2022',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14),
+                ),
+              ])),
           Padding(
-            padding: EdgeInsets.fromLTRB(50, 280, 0, 0),
+            padding: EdgeInsets.fromLTRB(15, 280, 0, 0),
             child: Text(
               'Status Pesanan',
               style: TextStyle(
                   color: Color(0xFF8D99AE),
                   fontSize: 16,
-                  fontWeight: FontWeight.w300),
+                  fontWeight: FontWeight.w600),
             ),
           ),
           Padding(
@@ -69,12 +108,24 @@ class _DashboardState extends State<Dashboard> {
           Padding(
               padding: EdgeInsets.fromLTRB(15, 330, 0, 0),
               child: Container(
-                color: Color(0xFF8D99AE),
                 height: 100,
                 width: 100,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color(0xFF8D99AE),
+                          spreadRadius: 0.1,
+                          blurRadius: 3,
+                          offset: Offset(0.0, 4.0))
+                    ]),
                 child: TextButton(
-                  child: Icon(Icons.add_to_home_screen_outlined,
-                      color: Colors.white),
+                  child: Icon(
+                    Icons.add_to_home_screen_outlined,
+                    color: SecondaryColor,
+                    size: 40,
+                  ),
                   onPressed: () {
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context) => track()));
@@ -84,11 +135,24 @@ class _DashboardState extends State<Dashboard> {
           Padding(
               padding: EdgeInsets.fromLTRB(140, 330, 0, 0),
               child: Container(
-                color: Color(0xFF8D99AE),
                 height: 100,
                 width: 100,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color(0xFF8D99AE),
+                          spreadRadius: 0.1,
+                          blurRadius: 3,
+                          offset: Offset(0.0, 4.0))
+                    ]),
                 child: TextButton(
-                  child: Icon(Icons.av_timer_outlined, color: Colors.white),
+                  child: Icon(
+                    Icons.av_timer_outlined,
+                    color: SecondaryColor,
+                    size: 40,
+                  ),
                   onPressed: () {
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context) => track()));
@@ -98,11 +162,24 @@ class _DashboardState extends State<Dashboard> {
           Padding(
               padding: EdgeInsets.fromLTRB(270, 330, 15, 0),
               child: Container(
-                color: Color(0xFF8D99AE),
                 height: 100,
                 width: 100,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color(0xFF8D99AE),
+                          spreadRadius: 0.1,
+                          blurRadius: 3,
+                          offset: Offset(0.0, 4.0))
+                    ]),
                 child: TextButton(
-                  child: Icon(Icons.checklist, color: Colors.white),
+                  child: Icon(
+                    Icons.checklist,
+                    color: SecondaryColor,
+                    size: 40,
+                  ),
                   onPressed: () {
                     Navigator.push(context,
                         MaterialPageRoute(builder: (context) => track()));
@@ -110,7 +187,7 @@ class _DashboardState extends State<Dashboard> {
                 ),
               )),
           Padding(
-            padding: EdgeInsets.fromLTRB(20, 435, 15, 0),
+            padding: EdgeInsets.fromLTRB(20, 440, 15, 0),
             child: RichText(
               text: TextSpan(children: [
                 TextSpan(
@@ -125,7 +202,7 @@ class _DashboardState extends State<Dashboard> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB(174, 435, 15, 0),
+            padding: EdgeInsets.fromLTRB(174, 440, 15, 0),
             child: RichText(
               text: TextSpan(children: [
                 TextSpan(
@@ -140,7 +217,7 @@ class _DashboardState extends State<Dashboard> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB(304, 435, 15, 0),
+            padding: EdgeInsets.fromLTRB(304, 440, 15, 0),
             child: RichText(
               text: TextSpan(children: [
                 TextSpan(
