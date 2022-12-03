5eb9e1d7719bf6da28c2d5a002d44c1d0b2d70ea
thoriqlukman <thoriqlh123@gmail.com>
2022-12-04 03:42:19 +0700

    thoriq

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -10,7 +10,6 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uzitailor/pages/tracking_home.dart';
 import 'package:http/http.dart' as http;
