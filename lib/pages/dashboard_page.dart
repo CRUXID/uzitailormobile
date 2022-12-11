a885039805ea789b43b725e924d6776fb6ab1b41
gabriellia02 <gabrielliaalfina2002@gmail.com>
2022-12-11 16:10:58 +0700

    Gabriellia

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -185,9 +185,7 @@ class _DashboardState extends State<Dashboard> {
                         borderRadius: BorderRadius.circular(15),
                         image: DecorationImage(
                           fit: BoxFit.cover,
-                          image: NetworkImage(
-                            "https://images.unsplash.com/photo-1607355739828-0bf365440db5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1444&q=80",
-                          ),
+                          image: AssetImage('assets/images/kemeja.jpg'),
                         ),
                       ),
                     ),
@@ -199,9 +197,7 @@ class _DashboardState extends State<Dashboard> {
                         borderRadius: BorderRadius.circular(15),
                         image: DecorationImage(
                           fit: BoxFit.cover,
-                          image: NetworkImage(
-                            "https://images.pexels.com/photos/2583852/pexels-photo-2583852.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-                          ),
+                          image: AssetImage('assets/images/jas_almet.jpg'),
                         ),
                       ),
                     ),
@@ -213,9 +209,43 @@ class _DashboardState extends State<Dashboard> {
                         borderRadius: BorderRadius.circular(15),
                         image: DecorationImage(
                           fit: BoxFit.cover,
-                          image: NetworkImage(
-                            "https://images.unsplash.com/photo-1584810359583-96fc3448beaa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
-                          ),
+                          image: AssetImage('assets/images/seragam_kerja.jpg'),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 15),
+                      width: 350,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/images/skaos.jpg'),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 15),
+                      width: 350,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/images/ukuran.jpg'),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 15),
+                      width: 350,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/images/ukuran-2.jpg'),
                         ),
                       ),
                     ),
